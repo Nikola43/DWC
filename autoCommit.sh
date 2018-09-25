1c8c2815e39e3d6526c8bb744f7a71b2124cca25
Paulo Soares <pauloxti@gmail.com>
2018-09-25 19:25:42 +0200

    autocommit

--- autoCommit.sh
+++ autoCommit.sh
@@ -3,24 +3,28 @@ cd $HOME/GitHub/DAW/
 git add .
 git commit -m "autocommit"
 git push
+echo "\n"
 
 cd $HOME/GitHub/DIW/
 git add .
 git commit -m "autocommit"
 git push
+echo "\n"
 
 cd $HOME/GitHub/DWC/
 git add .
 git commit -m "autocommit"
 git push
+echo "\n"
 
 cd $HOME/GitHub/DWS/
 git add .
 git commit -m "autocommit"
 git push
+echo "\n"
 
 cd $HOME/GitHub/HLC/
 git add .
 git commit -m "autocommit"
 git push
-
+echo "\n"
---
+++ autoPull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd $HOME/GitHub/DAW/
+git pull
+echo "\n"
+
+cd $HOME/GitHub/DIW/
+git pull
+echo "\n"
+
+cd $HOME/GitHub/DWC/
+git pull
+echo "\n"
+
+cd $HOME/GitHub/DWS/
+git pull
+echo "\n"
+
+cd $HOME/GitHub/HLC/
+git pull
+echo "\n"
