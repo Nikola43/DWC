95215403ac983488f8f809ab959f6ba46c0a94bd
Paulo Soares <pauloxti@gmail.com>
2018-09-25 10:26:33 +0200

    autocommit

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+cd /c/Users/Paulo\ Soares/GitHub/DAW/
 git add .
 git commit -m "autocommit"
 git push
+
+cd /c/Users/Paulo\ Soares/GitHub/DIW/
+git add .
+git commit -m "autocommit"
+git push
+
+cd /c/Users/Paulo\ Soares/GitHub/DWC/
+git add .
+git commit -m "autocommit"
+git push
+
+cd /c/Users/Paulo\ Soares/GitHub/DWS/
+git add .
+git commit -m "autocommit"
+git push
+
+cd /c/Users/Paulo\ Soares/GitHub/HLC/
+git add .
+git commit -m "autocommit"
+git push
+
