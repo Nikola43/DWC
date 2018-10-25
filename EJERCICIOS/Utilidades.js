1974a9109b99650f377f4d5f1bbbba3d935d391b
Carl Vuorinen <carl.vuorinen@gmail.com>
2018-10-25 12:44:13 +0200

    autocommit

--- EJERCICIOS/Utilidades.js
+++ EJERCICIOS/Utilidades.js
@@ -1,17 +1,5 @@
-
-
-//  En este fichero recogeremos nuestras utilidades javascript.
-
-//  CAJAS DE TEXTO (<input type="text" ...... >):
-  //  e => Es el parámetro del evento onkeypress.
-  
-
-
-
-//  Permitir SOLO números con decimales. Separador es la coma (,):
 function PermiteSoloNumerosDecimalesComaNoPrimeraPosSigno(e) {
   var c = e.charCode || window.event.keyCode;
-  //  Permitir el signo menos ( - ) sólo uno y en la primera posición:
   if (c == 45) {
 	if (this.value.indexOf("-") == -1)
 		this.value = "-" + this.value;
