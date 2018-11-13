ff5cf171b348c2780680c2802f9379b9889c2222
Paulo Soares <pauloxti@gmail.com>
2018-11-13 11:25:41 +0100

    autocommit

---
+++ autoCommit.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+cd $HOME/GitHub/DAW/
+git add .
+git commit -m "autocommit"
+git push
+echo " "
+
+cd $HOME/GitHub/DIW/
+git add .
+git commit -m "autocommit"
+git push
+echo " "
+
+cd $HOME/GitHub/DWC/
+git add .
+git commit -m "autocommit"
+git push
+echo " "
+
+cd $HOME/GitHub/DWS/
+git add .
+git commit -m "autocommit"
+git push
+echo " "
+
+cd $HOME/GitHub/HLC/
+git add .
+git commit -m "autocommit"
+git push
+echo " "
---
+++ autoPull.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cd $HOME/GitHub/DAW/
+git pull
+echo " "
+
+cd $HOME/GitHub/DIW/
+git pull
+echo " "
+
+cd $HOME/GitHub/DWC/
+git pull
+echo " "
+
+cd $HOME/GitHub/DWS/
+git pull
+echo " "
+
+cd $HOME/GitHub/HLC/
+git pull
+echo " "
