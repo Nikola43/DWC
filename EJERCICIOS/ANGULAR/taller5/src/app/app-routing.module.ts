796ba5302305b2d1b40daa041c6cd191159ccf46
Paulo Soares <pauloxti@gmail.com>
2019-02-18 13:32:55 +0100

    autocommit

--- EJERCICIOS/ANGULAR/taller5/src/app/app-routing.module.ts
+++ EJERCICIOS/ANGULAR/taller5/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
+import {OwnersComponent} from './components/owners/owners.component';
+import {VetsComponent} from './components/vets/vets.component';
+import {OwnersDetalleComponent} from './components/owners-detalle/owners-detalle.component';
 
-const routes: Routes = [];
+// rutas
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  }, {
+    path: 'owners',
+    component: OwnersComponent
+  }, {
+    path: 'vets',
+    component: VetsComponent
+  },
+  {
+    path: 'owners/:id',
+    component: OwnersDetalleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- EJERCICIOS/ANGULAR/taller5/src/app/app.module.ts
+++ EJERCICIOS/ANGULAR/taller5/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { OwnersComponent } from './components/owners/owners.component';
+import {HttpClientModule} from '@angular/common/http';
+import { VetsComponent } from './components/vets/vets.component';
+import { OwnersDetalleComponent } from './components/owners-detalle/owners-detalle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OwnersComponent,
+    VetsComponent,
+    OwnersDetalleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
